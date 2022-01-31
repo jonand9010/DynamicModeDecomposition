decc484c73cdc46630d00190273a8b5febd103e2
jonand9010 <jonas.andersson@live.com>
2022-01-31 20:12:00 +0100

    webcam image acquisition

--- data/record_webcam.py
+++ data/record_webcam.py
@@ -1,9 +1,25 @@
+from tkinter import W
 import cv2
 import time
 
-camera = cv2.VideoCapture(0)
-time.sleep(1)
-ret,reference_frame = camera.read() # returns a single frame as reference
+class WebcamImage:
+    def __init__(self):
+        pass
 
-cv2.imshow('Reference image',reference_frame) # display the captured image
-cv2.waitKey(0)
+    def record_image(self):
+        self.camera = cv2.VideoCapture(0)
+        time.sleep(2)
+        ret,frame = self.camera.read() # returns a single frame as reference
+        self.image = frame
+
+    def get_grayscale(self):
+        self.grayscale = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+
+
+webcam = WebcamImage()
+webcam.record_image()
+webcam.get_grayscale()
+
+import matplotlib.pyplot as plt
+plt.imshow(webcam.grayscale)
+plt.show()
